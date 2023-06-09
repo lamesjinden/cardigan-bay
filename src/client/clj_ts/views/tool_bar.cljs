fd42099a3ab85d181e00541d9e12546dce3624bf
james <7298563+lamesjinden@users.noreply.github.com>
2023-06-08 20:54:05 -0400

    Display tool_bar actions as button groups.

--- src/client/clj_ts/views/tool_bar.cljs
+++ src/client/clj_ts/views/tool_bar.cljs
@@ -5,45 +5,35 @@
 (defn tool-bar [db]
   (fn []
     (let [mode (-> @db :mode)]
-      [:div
-       {:class ["toolbar-container"]}
+      [:div.toolbar-container
        (condp = mode
 
          :editing
          [:div
           [:div
-           [:span {:class ["button-container"]}
-            [:button
-             {:class    ["big-btn" "image-button"]
-              :on-click (fn []
+           [:span.button-container
+            [:button.big-btn.big-btn-left
+             {:on-click (fn []
                           (mode/set-view-mode! db)
                           (page/cancel-async! db))}
-             [:div [:span {:class [:material-symbols-sharp :clickable]} "close"] "Cancel"]]
-            [:button
-             {:class    ["big-btn" "image-button"]
-              :on-click (fn []
+             [:span {:class [:material-symbols-sharp :clickable]} "close"]]
+            [:button.big-btn.big-btn-right
+             {:on-click (fn []
                           (mode/set-view-mode! db)
                           (page/save-page-async! db))}
-             [:div
-              [:span {:class [:material-symbols-sharp :clickable]} "save"] "Save"]]]]]
+             [:span {:class [:material-symbols-sharp :clickable]} "save"]]]]]
 
          :viewing
-         [:span {:class ["button-container"]}
-          [:button
-           {:class    ["big-btn" "image-button"]
-            :on-click #(swap! db assoc :mode :editing)}
-           [:div
-            [:span {:class [:material-symbols-sharp :clickable]} "edit"] [:span "Edit"]]]
-          [:button
-           {:class ["big-btn" "image-button"]}
-           [:div
-            [:a {:href (str "/api/exportpage?page=" (-> @db :current-page))}
-             [:span {:class [:material-symbols-sharp :clickable]} "deployed_code_update"]
-             [:span "Export"]]]]]
+         [:span.button-container
+          [:button.big-btn.big-btn-left
+           {:on-click #(swap! db assoc :mode :editing)}
+           [:span {:class [:material-symbols-sharp :clickable]} "edit"]]
+          [:button.big-btn.big-btn-right
+           [:a {:href (str "/api/exportpage?page=" (-> @db :current-page))}
+            [:span {:class [:material-symbols-sharp :clickable]} "deployed_code_update"]]]]
 
          :transcript
-         [:span {:class ["button-container"]}
-          [:button
-           {:class    ["big-btn" "image-button"]
-            :on-click #(mode/set-view-mode! db)}
-           [:div [:span {:class [:material-symbols-sharp :clickable]} "close"] "Cancel"]]])])))
+         [:span.button-container
+          [:button.big-btn
+           {:on-click #(mode/set-view-mode! db)}
+           [:span {:class [:material-symbols-sharp :clickable]} "close"]]])])))
